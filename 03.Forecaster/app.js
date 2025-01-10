0b2af5e36f2d67f5d6c0024505b5ada6a3a786cc
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2025-01-10 12:47:57 +0200

    render data

--- 03.Forecaster/app.js
+++ 03.Forecaster/app.js
@@ -3,6 +3,8 @@ function attachEvents() {
     const submitInputElement = document.getElementById('submit');
     const locationInputElement = document.getElementById('location');
     const forecastElement = document.getElementById('forecast');
+    const currentElement = document.getElementById('current');
+    const upcomingElement = document.getElementById('upcoming');
 
     submitInputElement.addEventListener('click', onSubmit);
 
@@ -11,34 +13,95 @@ function attachEvents() {
         fetch(`${baseUrl}/locations`)
         .then(response => response.json())
         .then((locationsData) => {
-            console.log(locationsData);
-            console.log(locationName);
-            for (let locationData of locationsData) {
-                if (locationName !== `${locationData.name}`) {
-                    getInfoToday(locationData);
-                    getInfoUpcomingDay(locationData);
-                }
+            const location = locationsData.find(location => location.name === locationName);
+            if (!location) {
+                forecastElement.textContent = 'Please enter a location.';
+                return;
             }
+                    getInfoToday(location);
+                    getInfoUpcomingDay(location);
+
         })
     }
 
-    function getInfoToday(locationData) {
-        fetch(`${baseUrl}/today/${locationData.code}`)
+    function getInfoToday(location) {
+        fetch(`${baseUrl}/today/${location.code}`)
         .then(response => response.json())
         .then(todayData => {
             console.log(todayData);
 
+            const div = document.createElement('div');
+            div.className = 'forecasts';
+
+            const conditionSpan = document.createElement('span');
+            conditionSpan.className = 'condition symbol';
+            conditionSpan.innerHTML = getWeatherSymbol(todayData.forecast.condition);
+
+            const detailsSpan = document.createElement('span');
+            detailsSpan.className = 'condition';
+
+            const nameSpan = document.createElement('span');
+            nameSpan.className = 'forecast-data';
+            nameSpan.innerHTML = todayData.name;
+
+            const tempSpan = document.createElement('span');
+            tempSpan.className = 'forecast-data';
+            tempSpan.innerHTML = `${todayData.forecast.low}°/${todayData.forecast.high}°`;
+
+            const conditionTextSpan = document.createElement('span');
+            conditionTextSpan.className = 'forecast-data';
+            conditionTextSpan.innerHTML = todayData.forecast.condition;
+
+            detailsSpan.append(nameSpan, tempSpan, conditionTextSpan);
+            div.append(conditionSpan, detailsSpan);
+            currentElement.append(div);
+
         })
     }
 
-    function getInfoUpcomingDay(locationData) {
-        fetch(`${baseUrl}/upcoming/${locationData.code}`)
+    function getInfoUpcomingDay(location) {
+        fetch(`${baseUrl}/upcoming/${location.code}`)
             .then(response => response.json())
             .then(upcomingData => {
                 console.log(upcomingData);
 
+                const div = document.createElement('div');
+                div.className = 'forecast-info';
+
+                upcomingData.forecast.forEach(day => {
+                    const daySpan = document.createElement('span');
+                    daySpan.className = 'upcoming';
+
+                    const conditionSymbol = document.createElement('span');
+                    conditionSymbol.className = 'symbol';
+                    conditionSymbol.innerHTML = getWeatherSymbol(day.condition);
+
+                    const tempSpan = document.createElement('span');
+                    tempSpan.className = 'forecast-data';
+                    tempSpan.innerHTML = `${day.low}°/${day.high}°`;
+
+                    const conditionTextSpan = document.createElement('span');
+                    conditionTextSpan.className = 'forecast-data';
+                    conditionTextSpan.innerHTML = day.condition;
+
+                    daySpan.append(conditionSymbol, tempSpan, conditionTextSpan);
+                    div.append(daySpan);
+                });
+
+                upcomingElement.append(div);
             });
 
+    }
+
+    function getWeatherSymbol(condition) {
+        const symbols = {
+            'Sunny': '&#x2600',
+            'Partly sunny': '&#x26C5',
+            'Overcast': '&#x2601',
+            'Rain': '&#x2614',
+            'Snow': '&#176',
+        };
+        return symbols[condition] || '';
     }
 
     function onSubmit(e) {
